dbb09a9f8f8d33ff56c5f167b3d4c79d74496280
Alejandro Brown Arnold <abrown@lasegunda.com.ar>
2023-03-26 18:19:48 -0300

    Add config for goerli network connection and deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,9 +6,13 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
-    test: {
+    goerli: {
       url: process.env.GOERLI_URL,
       accounts: [`0x${process.env.PRIVATE_KEY}`]
+    },
+    test: {
+      url: process.env.NODE_IP,
+      accounts: [`0x${process.env.TESTPRIVATE_KEY}`]
     }
   }
 };
